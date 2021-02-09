515280d4fc663f4fe156a207c77caadb94ae3afa
etamar <etamar.levy@gmail.com>
2021-02-09 11:17:26 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,65 @@
+function add() {
+     1 + 2;
+}
+
+function subtract() {
+    4 - 2;
+}
+
+function multiply() {
+    2 *	3;
+}
+
+function divide() {
+    6 / 2;
+}
+
+function add(x, y) {
+    return x + y;
+  } 
+
+  function subtract(x, y) {
+      return x - y;
+  }
+
+  function multiply(a, b) {
+    return a * b;
+  }
+
+  function divide(a, b) {
+      return a / b;
+  }
+
+function increment(n) {
+    return n += 1;
+}
+
+function decrement(n) {
+    return n -= 1;
+}
+
+function makeInt(n) {
+    const parsed = parseInt(n, 10);
+}
+
+function makeInt(n) {
+    return parseInt(n);
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n, 2.22);
+}
+
+
+
+
+
+
+
+
+
+
